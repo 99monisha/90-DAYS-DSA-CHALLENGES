25d95333ee771adcc9e57346e0c421f83beaf41c
Monisha Dolai <monishadolai1998@gmail.com>
2022-06-29 12:03:05 +0530

    smallest and largest

--- ARRAY/smallest and largest/smalllarge.cpp
+++ ARRAY/smallest and largest/smalllarge.cpp
@@ -11,13 +11,10 @@ int main()
     int largest=INT_MIN;
     for(i=0;i<n;i++)
     {
-        if(a[i]<smallest)
-        {
+        if(a[i]>smallest)
         smallest=a[i];
-        }
-        if(a[i]>largest){
+        if(a[i]<largest)
         largest=a[i];
-        }
     }
     cout<<smallest<<" "<<largest;
     return 0;
