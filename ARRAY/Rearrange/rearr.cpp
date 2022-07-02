7af322bcd4888cf251280a07e9bc01825fcbb34a
Monisha Dolai <monishadolai1998@gmail.com>
2022-07-02 19:45:18 +0530

    rearrange positive negative

---
+++ ARRAY/Rearrange/rearr.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void rearrangr(int a[],int n)
+{
+    int j=0;
+    for(int i=0;i<n;i++)
+    {
+    if(a[i]<0)
+    {
+        if(i!=0)
+        
+            swap(a[i],a[j]);
+            j++;
+        
+    }
+    }
+}
+void print(int a[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+int main()
+{
+    int n,i;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    print(a,n);
+    rearrangr(a,n);
+    print(a,n);
+    return 0;
+}
