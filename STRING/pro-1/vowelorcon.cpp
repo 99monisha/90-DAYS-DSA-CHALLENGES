d05c5bddf49afea4c762526ed3315ceaa05108ed
Monisha Dolai <monishadolai1998@gmail.com>
2022-07-26 18:09:55 +0530

    checking vowel or consonent

---
+++ STRING/pro-1/vowelorcon.cpp
@@ -0,0 +1,16 @@
+/*
+U is a vowel
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c;
+    c='U';
+    if(c=='a' || c=='e'|| c=='i' ||c=='o' || c=='u' ||c=='e'||  c=='A' ||c=='E'|| c=='I' ||c=='O'|| c=='U')
+    cout<< c <<" is a vowel";
+    else
+    cout<< c<< " is not a vowel";
+    return 0;
+}
