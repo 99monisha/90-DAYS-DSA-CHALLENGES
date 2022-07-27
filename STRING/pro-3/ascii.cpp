7f0b77167a4d44238da202988f12c92e564564fa
Monisha Dolai <monishadolai1998@gmail.com>
2022-07-27 10:08:54 +0530

    ASCII value

---
+++ STRING/pro-3/ascii.cpp
@@ -0,0 +1,13 @@
+/*Print ascii value of a character
+a
+The ASCII value of a is 97
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cin>>ch;
+    cout<<"The ASCII value of "<<ch<<" is "<<(int)ch;
+    return 0;
+}
