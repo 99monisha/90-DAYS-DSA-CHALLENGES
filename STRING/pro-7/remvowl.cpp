f0fc23ccbb4ba15440435a3456dce0f5ca48d5d2
Monisha Dolai <monishadolai1998@gmail.com>
2022-07-29 20:54:51 +0530

    vowel remove

---
+++ STRING/pro-7/remvowl.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch[100];
+    cout<<"Enter String: ";
+    cin>>ch;
+    int length=strlen(ch);
+    for(int i=0;i<length;i++)
+    {
+        if(ch[i]=='a' || ch[i]=='e' || ch[i]=='i' || ch[i]=='o' || ch[i]=='u' ||
+          ch[i]=='A' || ch[i]=='E' || ch[i]=='I' || ch[i]=='O' || ch[i]=='U')
+          {
+             for(int j=i;j<length;j++)
+             {
+                ch[j]=ch[j+1];
+             }
+             i--;
+             length--;
+          }
+    }
+    cout<<"After removing vowels: "<<ch;
+    return 0;
+}
