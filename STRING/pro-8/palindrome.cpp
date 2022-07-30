09a634d859156e34ec13f1144ad988bef208f22d
Monisha Dolai <monishadolai1998@gmail.com>
2022-07-30 23:22:14 +0530

    string is palindrome or not

--- STRING/pro-8/palindrome.cpp
+++ STRING/pro-8/palindrome.cpp
@@ -1,15 +1,41 @@
+/*
+The String is: madam palindrome
+The String is: papa not palindrome
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+void lower(char ch[])
 {
-    char ch;
-     int i;
-    cout<<"Enter the String: ";
-    cin>>ch;
-   
-    for(i=0;ch[i]!='\0';i++)
+    int i=0;
+    while (ch[i]!='\0')
     {
-
+        if(ch[i]>64 && ch[i]<91)
+ch[i]+=32;
+i++;
     }
+    
+}
+void palin(char ch[])
+{
+    int l=0;int h=strlen(ch)-1;
+    lower(ch);
+    while(h>l)
+    {
+        if(ch[l++]!=ch[h--])
+        {
+            cout<<"The String is: "<<ch<<" not palindrome";
+            return;
+        }
+    }
+    cout<<"The String is: "<<ch<<" palindrome";
+}
+int main(){
+char ch1[50]="madam";
+palin(ch1);
+cout<<endl;
+char ch2[50]="papa";
+palin(ch2);
+
     return 0;
 }
