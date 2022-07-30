7d9c903ff2e9db0953833c58dc2a6e8fc5331963
Monisha Dolai <monishadolai1998@gmail.com>
2022-07-30 23:11:53 +0530

    ..

---
+++ STRING/pro-8/palindrome.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch;
+     int i;
+    cout<<"Enter the String: ";
+    cin>>ch;
+   
+    for(i=0;ch[i]!='\0';i++)
+    {
+
+    }
+    return 0;
+}
