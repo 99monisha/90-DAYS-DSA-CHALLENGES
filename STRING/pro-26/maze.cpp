bca1a108cadb973e7f01fb1d35612324c3d3834b
Monisha Dolai <monishadolai1998@gmail.com>
2022-08-10 21:33:50 +0530

    possible in a maze

--- STRING/pro-26/maze.cpp
+++ STRING/pro-26/maze.cpp
@@ -1,12 +1,16 @@
+/*
+6
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int countpath(int n, int i, int j)
 {
     if(i==n-1 && j==n-1)
     {
-        return -1;
+        return 1;
     }
-    if(i>=i+1,j)
+    if(i>=n || j>=n)
     {
         return 0;
     }
