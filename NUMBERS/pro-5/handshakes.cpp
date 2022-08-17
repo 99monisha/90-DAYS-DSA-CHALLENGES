5af8eb1addc45b03601ce012851d04e5bcdbbd89
Monisha Dolai <monishadolai1998@gmail.com>
2022-08-17 19:21:18 +0530

    handshake

---
+++ NUMBERS/pro-5/handshakes.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num=9;
+    int total=num*(num-1)/2;
+    cout<<"For "<<num<<" people there will "<<total<<" handshakes ";
+    return 0;
+}
