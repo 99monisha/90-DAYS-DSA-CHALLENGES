6300d503db5942e2dd065b9aa0c2e5a94c37bfaa
Monisha Dolai <monishadolai1998@gmail.com>
2022-08-26 19:52:50 +0530

    numbers of digits

---
+++ NUMBERS/pro-10/num.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int num = 20901;
+  int digit = floor (log10 (num)) + 1;
+  cout << "No. of digits = " << digit;
+   return 0;
+ 
+}
