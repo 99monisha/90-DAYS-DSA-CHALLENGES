af411d925dbd7ef04ff0aee3d09f285f05f4e06c
Monisha Dolai <monishadolai1998@gmail.com>
2022-08-28 08:55:31 +0530

    x digits occurs

---
+++ NUMBERS/pro-12/xdigt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n = 890190798; 
+    int d = 9; 
+
+    int count=0; 
+
+    while(n) {
+
+        int rem = n%10; 
+        if(rem == d){
+            count++;
+        }
+        n=n/10; 
+    }
+
+    cout<<count;
+
+    return 0;
+
+}
