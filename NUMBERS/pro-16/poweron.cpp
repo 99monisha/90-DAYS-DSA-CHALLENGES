774e847890490bc2404f34714dabdc7a38dc0312
Monisha Dolai <monishadolai1998@gmail.com>
2022-08-31 12:26:15 +0530

    power one

---
+++ NUMBERS/pro-16/poweron.cpp
@@ -0,0 +1,20 @@
+  
+  
+  #include<bits/stdc++.h>
+  using namespace std;
+
+ int power(int base, int x){
+
+  int result=1;
+  while(x--){
+  result *= base;
+ }
+
+ return result;
+}
+  int main()
+  {
+  int base = 5, x = 3;
+  cout<<"Required Power is "<<power(base, x);
+  return 0;
+}
