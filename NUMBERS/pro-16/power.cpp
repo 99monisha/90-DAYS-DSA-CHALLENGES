e1b6b8cf83295b1e876b529df9046a872fde9f59
Monisha Dolai <monishadolai1998@gmail.com>
2022-08-31 12:19:58 +0530

    power

---
+++ NUMBERS/pro-16/power.cpp
@@ -0,0 +1,13 @@
+ #include<bits/stdc++.h>
+  using namespace std;
+  int power(int base, int x){
+
+  if(x==0)
+    return 1;
+
+  return (base*power(base, x-1));
+ }
+  int main(){
+  int base = 5, x = 3;
+  cout<<"Required Power is "<<power(base, x);
+}
