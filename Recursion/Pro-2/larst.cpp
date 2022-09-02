ea01f926be6b65f23ae2cae51ab5471fd63e6f5e
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-02 11:43:28 +0530

    largest number in array

--- Recursion/Pro-2/larst.cpp
+++ Recursion/Pro-2/larst.cpp
@@ -12,13 +12,11 @@ int max_element = INT_MIN;
 for(int i=0; i<n; i++){
  max_element = max(arr[i], max_element);
    }
-
-   return max_element;
+return max_element;
 }
 
 int main(){
-
-   int arr[] = {10, 45, 78, 34, 67};
+int arr[] = {10, 45, 78, 34, 67};
    int n = sizeof(arr)/sizeof(arr[0]);
    cout<<"Largest Element is : "<<largest_element(n, arr);
 }
