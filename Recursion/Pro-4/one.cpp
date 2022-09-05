3c1db102b9c08fc1cb17130f284c866a0a9e4029
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-05 12:58:11 +0530

    ..

---
+++ Recursion/Pro-4/one.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int smallest_element(int n, int arr[]){
+
+   if(n==1) return arr[0];
+
+   return min(arr[n-1], smallest_element(n-1, arr));
+}
+int main(){
+
+   int arr[] = {10, 45, 78, 34, 67};
+
+   int n = sizeof(arr)/sizeof(arr[0]);
+
+   cout<<"Smallest Element is "<<smallest_element(n, arr);
+}
