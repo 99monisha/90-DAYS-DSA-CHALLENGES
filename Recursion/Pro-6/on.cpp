610126b0dccae9d3a13fc08042458b496f2f6add
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-07 17:51:56 +0530

    mm

---
+++ Recursion/Pro-6/on.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int Len(char* str) 
+{
+   if (*str == '\0')
+      return 0;
+   else
+      return 1 + Len(str + 1);
+}
+int main()
+{
+   char str[] = "PrepInsta";
+   cout << Len(str);
+   return 0;
+}
