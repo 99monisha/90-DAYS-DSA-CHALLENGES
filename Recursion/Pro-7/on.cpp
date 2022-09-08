b223db37bf6d26a48a8a87fc957c377befedd6be
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-08 12:05:11 +0530

    fact

---
+++ Recursion/Pro-7/on.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num = 6, fact = 1;
+    if(num < 0)
+        cout << "Not Possible";
+    else
+    {
+        for(int i = 1; i <= num; i++)
+            fact = fact * i;
+    }
+    
+    cout << "Fact " << num << ": " << fact;
+}
