7edbdbdf121028e1afa02c727ed17ffe3c5e451f
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-08 12:07:31 +0530

    m2

---
+++ Recursion/Pro-7/tw.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int getFactorial(int num)
+{
+    if(num == 0)
+        return 1;
+        
+    return num * getFactorial(num-1);
+}
+int main ()
+{
+    int num = 8;
+    
+    int fact = getFactorial(num);
+    
+    cout << "Fact " << num << " : " << fact;
+    
+    return 0;
+}
