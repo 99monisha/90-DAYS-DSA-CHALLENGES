b615ab89c6d09614a7748755227561d81e862584
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-12 11:37:16 +0530

    search element

---
+++ Matrix/Pro-3/tw.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   int n;
+   cin>>n;
+
+   int a[n][n];
+   for(int i=0; i>a[i][j];
+   }
+
+  int search;
+  cin>>search;
+
+  bool flag=0;
+  int i=0, j=n-1;
+  
+  while (i < n && j >= 0)
+    {
+       if (a[i][j] == search)
+        {
+           cout<<"Element is found at ("< search)
+            j--;
+        else
+            i++;
+    }
+
+  if(flag==0)
+  cout<<"Not found";
+}
