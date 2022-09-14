5ce4d1c52ed840719210073e07d888f27b0ebb1b
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-14 13:26:53 +0530

    median

---
+++ Matrix/Pro-5/on.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int mat[3][3] = { {1, 3, 5}, {2, 6, 9}, {3, 6, 9}};
+    int arr[9], x=0;
+    for(int i=0; i<3; i++){
+       for(int j=0; j<3; j++){
+           arr[x++] = mat[i][j];
+       }
+    }
+
+    sort(arr, arr+9);
+    cout << "Median of the given matrix is : " << arr[4];
+}
