2f6023bb41b76934505ede4fd51b2ec64e05d3d4
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-14 13:32:44 +0530

    median

---
+++ Matrix/Pro-5/tw.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int r = 3, c = 3;
+      int m[3][3]= { {1,3,5}, {2,6,9}, {3,6,9}};
+      int min = INT_MAX, max = INT_MIN;
+
+      for (int i=0; i< r; i++)
+     {
+           if (m[i][0] < min) min = m[i][0]; if (m[i][c-1] > max)
+              max = m[i][c-1];
+     }
+
+     int desired = (r * c + 1) / 2;
+     while (min < max)
+     {
+            int mid = min + (max - min) / 2;
+            int place = 0;
+
+            for (int i = 0; i < r; ++i)
+                    place += upper_bound(m[i], m[i]+c, mid) - m[i];
+                    if (place < desired)
+                        min = mid + 1;
+                    else
+                        max = mid;
+      }
+      cout << "Median is " << min ;
+      return 0;
+}
