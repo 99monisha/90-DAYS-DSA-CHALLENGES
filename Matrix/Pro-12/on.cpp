64882f4f061457365b4f3250974da1caf36ea7d0
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-21 10:26:48 +0530

    jkhj

---
+++ Matrix/Pro-12/on.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMedian(int ar1[], int ar2[], int n)
+{
+    int j = 0;
+    int i = n - 1;
+    while (ar1[i] > ar2[j] && j < n && i > -1)
+        swap(ar1[i--], ar2[j++]);
+
+    sort(ar1, ar1 + n);
+    sort(ar2, ar2 + n);
+
+    return (ar1[n - 1] + ar2[0]) / 2;
+}
+int main()
+{
+   int n;
+   cin>>n;
+
+   int arr1[n], arr2[n];
+   for(int i=0; i<n; i++)
+      cin>>arr1[i];
+
+   for(int i=0; i<n; i++)
+      cin>>arr2[i];
+
+   cout<<getMedian(arr1, arr2, n);
+
+   return 0;
+}
