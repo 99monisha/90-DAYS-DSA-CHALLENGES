f246fe7875fc5b35c3082e187f0f3c6d8caf7063
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-23 00:04:32 +0530

    jjj

---
+++ Matrix/Pro-14/tw.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  string s = "abc" ;
+
+  bool val= next_permutation(s.begin(), s.end());
+
+  if (val == false)
+   cout << "No Word Possible" << endl;
+  else
+   cout << s << endl;
+
+  return 0;
+}
