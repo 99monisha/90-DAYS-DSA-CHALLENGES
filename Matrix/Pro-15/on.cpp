016532ba770dd0e9904f9f4bc0e6f674193b3a33
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-24 13:09:37 +0530

    bnb

--- Matrix/Pro-15/on.cpp
+++ Matrix/Pro-15/on.cpp
@@ -15,17 +15,6 @@ int maxWater(int arr[], int n)
     for (int i = 1; i < n-1; i++) {
       int left = arr[i];
       for (int j=0; j< i; j++)
-        left = max(left, arr[j]); 
-      int right = arr[i];
-      for (int j=i+1; j< n; j++) 
-      right = max(right, arr[j]);
-using namespace std;
-int maxWater(int arr[], int n)
-{
-    int res = 0;
-    for (int i = 1; i < n-1; i++) {
-      int left = arr[i];
-      for (int j=0; j< i; j++)
         left = max(left, arr[j]);
       int right = arr[i];
       for (int j=i+1; j< n; j++) 
