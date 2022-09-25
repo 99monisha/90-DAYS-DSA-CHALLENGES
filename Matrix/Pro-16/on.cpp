da119befc40df4c78b9bb2e5eff0299eeed112a6
Monisha Dolai <monishadolai1998@gmail.com>
2022-09-25 14:16:35 +0530

    jknj

---
+++ Matrix/Pro-16/on.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main()
+{
+  int n1;
+  scanf("%d", &n1);
+
+  int ar1[n1];
+  for(int i=0; i<n1; i++) 
+     scanf("%d", &ar1[i]);
+
+  int n2;
+  scanf("%d", &n2);
+
+  int ar2[n2];
+  for(int i=0; i<n2; i++) 
+    scanf("%d", &ar2[i]);
+
+  int n3;
+  scanf("%d", &n3);
+
+  int ar3[n3];
+  for(int i=0; i<n3; i++) 
+    scanf("%d", &ar3[i]);
+
+  printf("Common Elements are ");
+
+  for(int i=0; i<n1; i++){
+
+      int flag = 0;
+      for(int j=0 ; j<n2; j++){
+
+         if(ar1[i]==ar2[j])
+         { 
+
+            for(int k=0; k<n3; k++){
+
+              if(ar2[j]==ar3[k])
+              {
+                flag=1;
+                k++;
+                break;
+              }
+           }
+
+           j++;
+           break;
+          }
+     }
+
+     if(flag)
+     printf("%d ", ar1[i]);
+  }
+return 0;
+}
